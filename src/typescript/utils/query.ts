2376f1d3bea6384d4707c8c3403bebac7015bf98
Patrick Janssen <akiphtj@nus.edu.sg>
2017-11-01 14:23:24 +0800

    Reorganised folder structure

---
+++ src/typescript/utils/query.ts
@@ -0,0 +1,11 @@
+//function for querying the gs-JSON dataset
+
+export function numPoints(model: GSJSON):number {
+    //return number of points
+    return 5;
+}
+
+export function numVertices(model: GSJSON):number {
+    //return number of points
+    return 5;
+}
