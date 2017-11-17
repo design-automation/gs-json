1b4fb5e403a5f1ae5a62bd94cec79d0d2b62fffb
Mastertype <33281683+Mastertype@users.noreply.github.com>
2017-11-17 11:23:04 +0800

    Update entities_tests.ts

--- src/typescript/gs-json/utils/entities_tests.ts
+++ src/typescript/gs-json/utils/entities_tests.ts
@@ -14,3 +14,43 @@ export function test_setPointPosition():boolean {
     let pos = point.getPosition();
     return Arr.equal([4,5,6], pos);
 }
+
+let data: ifs.IModelData = {
+    "metadata": {
+            "filetype": "mobius",
+            "version": 0.1,
+            "crs": { "epsg": 3857 },
+            "location": "+40.6894-074.0447"
+        },
+    "geometry": [
+        [
+            [[16, 17, 18, 19, 20, 21]],
+            [],
+            [100]
+        ]
+    ],
+    "attribs": [
+        {
+            "name": "position",
+            "topo_type": "points",
+            "data_type": "number[]",
+            "map": [0, 1, 2, 3],
+            "values": [[-0.7794438004493713, -1.0, 0.0], [0.22055619955062866, -1.0, 0.0], [0.22055619955062866, -1.0, 3.0]]
+        }
+    ],
+    "groups": [
+        {
+            "name": "boxes",
+            "objs": [[6], [7]]
+        }
+    ]
+};
+
+export function test1():void{
+    let m1:Model = new Model();
+    m1.setData(data);
+    let e1:Entity = new Entity(m1.getGeom()); //constructor
+    e1.getGeom(); //method 1
+    e1.getModel(); //method 2
+}
+test1() //OK
