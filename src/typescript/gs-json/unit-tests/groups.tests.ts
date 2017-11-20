7f0d37caec0644dd7f77e770cc3a9cddb8cc5d88
Kamel Adouane <sdeka@nus.edu.sg>
2017-11-20 17:53:40 +0800

    adding test structure to Groups

--- src/typescript/gs-json/unit-tests/groups.tests.ts
+++ src/typescript/gs-json/unit-tests/groups.tests.ts
@@ -2,10 +2,61 @@ import {} from 'jasmine';
 import * as groups_tests from "../utils/groups_tests";
 
 describe('Tests for Group class', () => {
-
     it('test_group_methods', () => {
         expect( groups_tests.test_group_methods() ).toBe(true);
     });
-
-});	
-
+	it('test_Groups_constructor', () => {
+        expect( groups_tests.test_Groups_constructor() ).toBe(true);
+    });
+	it('test_Groups_getName', () => {
+        expect( groups_tests.test_Groups_getName() ).toBe(true);
+    });
+	it('test_Groups_setName', () => {
+        expect( groups_tests.test_Groups_setName() ).toBe(true);
+    });
+	it('test_Groups_getParentGroup', () => {
+        expect( groups_tests.test_Groups_getParentGroup() ).toBe(true);
+    });
+	it('test_Groups_getChildGroups', () => {
+        expect( groups_tests.test_Groups_getChildGroups() ).toBe(true);
+    });
+	it('test_Groups_setParentGroup', () => {
+        expect( groups_tests.test_Groups_setParentGroup() ).toBe(true);
+    });
+	it('test_Groups_removeParentGroup', () => {
+        expect( groups_tests.test_Groups_removeParentGroup() ).toBe(true);
+    });
+	it('test_Groups_getPointIDs', () => {
+        expect( groups_tests.test_Groups_getPointIDs() ).toBe(true);
+    });
+	it('test_Groups_addPoint', () => {
+        expect( groups_tests.test_Groups_addPoint() ).toBe(true);
+    });
+	it('test_Groups_addPoints', () => {
+        expect( groups_tests.test_Groups_addPoints() ).toBe(true);
+    });
+	it('test_Groups_removePoint', () => {
+        expect( groups_tests.test_Groups_removePoint() ).toBe(true);
+    });
+	it('test_Groups_removePoints', () => {
+        expect( groups_tests.test_Groups_removePoints() ).toBe(true);
+    });
+	it('test_Groups_getObjIDs', () => {
+        expect( groups_tests.test_Groups_getObjIDs() ).toBe(true);
+    });
+	it('test_Groups_addObj', () => {
+        expect( groups_tests.test_Groups_addObj() ).toBe(true);
+    });
+	it('test_Groups_addObjs', () => {
+        expect( groups_tests.test_Groups_addObjs() ).toBe(true);
+    });
+	it('test_Groups_removeObj', () => {
+        expect( groups_tests.test_Groups_removeObj() ).toBe(true);
+    });
+	it('test_Groups_removeObjs', () => {
+        expect( groups_tests.test_Groups_removeObjs() ).toBe(true);
+    });
+	it('test_Groups_getPropeties', () => {
+        expect( groups_tests.test_Groups_getPropeties() ).toBe(true);
+    });
+});
--- src/typescript/gs-json/utils/groups_tests.ts
+++ src/typescript/gs-json/utils/groups_tests.ts
@@ -15,3 +15,60 @@ let Model_1:gsj.Model = new gsj.Model();
     Model_1.getGroups(); // Get Groups show two names per group
     return true;
 }
+
+
+// Testing methods the Groups Class, composed of 1 constructor and 17 methods
+export function test_Groups_constructor():boolean{
+	return true;
+}
+export function test_Groups_getName():boolean{
+	return true;
+}
+export function test_Groups_setName():boolean{
+	return true;
+}
+export function test_Groups_getParentGroup():boolean{
+	return true;
+}
+export function test_Groups_getChildGroups():boolean{
+	return true;
+}
+export function test_Groups_setParentGroup():boolean{
+	return true;
+}
+export function test_Groups_removeParentGroup():boolean{
+	return true;
+}
+export function test_Groups_getPointIDs():boolean{
+	return true;
+}
+export function test_Groups_addPoint():boolean{
+	return true;
+}
+export function test_Groups_addPoints():boolean{
+	return true;
+}
+export function test_Groups_removePoint():boolean{
+	return true;
+}
+export function test_Groups_removePoints():boolean{
+	return true;
+}
+export function test_Groups_getObjIDs():boolean{
+	return true;
+}
+export function test_Groups_addObj():boolean{
+	return true;
+}
+export function test_Groups_addObjs():boolean{
+	return true;
+}
+export function test_Groups_removeObj():boolean{
+	return true;
+}
+export function test_Groups_removeObjs():boolean{
+	return true;
+}
+export function test_Groups_getPropeties():boolean{
+	return true;
+}
