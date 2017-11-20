3a3e3a904edbd83bdac98984937a6465178cf8b1
Kamel Adouane <sdeka@nus.edu.sg>
2017-11-20 18:52:54 +0800

    Topo testing structure

--- src/typescript/gs-json/unit-tests/topos.tests.ts
+++ src/typescript/gs-json/unit-tests/topos.tests.ts
@@ -1,28 +1,100 @@
 import {} from 'jasmine';
 import * as topos_tests from "../utils/topos_tests";
 
-describe('Tests for Entity class', () => {
-
-});	
-
 describe('Tests for Topo class', () => {
-
-});	
+    it('test_Topo_constructor', () => {
+        expect( topos_tests.test_Topo_constructor() ).toBe(true);
+    });
+    it('test_Topo_getObjID', () => {
+        expect( topos_tests.test_Topo_getObjID() ).toBe(true);
+    });
+    it('test_Topo_getGeom', () => {
+        expect( topos_tests.test_Topo_getGeom() ).toBe(true);
+    });
+    it('test_Topo_getModel', () => {
+        expect( topos_tests.test_Topo_getModel() ).toBe(true);
+    });
+    it('test_Topo_getGeomType', () => {
+        expect( topos_tests.test_Topo_getGeomType() ).toBe(true);
+    });
+    it('test_Topo_getAttribNames', () => {
+        expect( topos_tests.test_Topo_getAttribNames() ).toBe(true);
+    });
+    it('test_Topo_getAttribValue', () => {
+        expect( topos_tests.test_Topo_getAttribValue() ).toBe(true);
+    });
+    it('test_Topo_setAttribValue', () => {
+        expect( topos_tests.test_Topo_setAttribValue() ).toBe(true);
+    });
+    it('test_Topo_getGroupNames', () => {
+        expect( topos_tests.test_Topo_getGroupNames() ).toBe(true);
+    });
+});
 
 describe('Tests for Vertex class', () => {
-
+	it('test_Vertex_getGeomType', () => {
+        expect( topos_tests.test_Vertex_getGeomType() ).toBe(true);
+    });
+	it('test_Vertex_getPoint', () => {
+        expect( topos_tests.test_Vertex_getPoint() ).toBe(true);
+    });
+	it('test_Vertex_next', () => {
+        expect( topos_tests.test_Vertex_next() ).toBe(true);
+    });
+	it('test_Vertex_previous', () => {
+        expect( topos_tests.test_Vertex_previous() ).toBe(true);
+    });
+	it('test_Vertex_getEdge', () => {
+        expect( topos_tests.test_Vertex_getEdge() ).toBe(true);
+    });
 });	
 
 describe('Tests for Edge class', () => {
-
+	it('test_Edge_getGeomType', () => {
+        expect( topos_tests.test_Edge_getGeomType() ).toBe(true);
+    });
+	it('test_Edge_getVertices', () => {
+        expect( topos_tests.test_Edge_getVertices() ).toBe(true);
+    });
+	it('test_Edge_next', () => {
+        expect( topos_tests.test_Edge_next() ).toBe(true);
+    });
+	it('test_Edge_previous', () => {
+        expect( topos_tests.test_Edge_previous() ).toBe(true);
+    });
+	it('test_Edge_neighbours', () => {
+        expect( topos_tests.test_Edge_neighbours() ).toBe(true);
+    });
+	it('test_Edge_getParent', () => {
+        expect( topos_tests.test_Edge_getParent() ).toBe(true);
+    });    
 });
 
 describe('Tests for Wire class', () => {
-
+	it('test_Wire_getGeomType', () => {
+        expect( topos_tests.test_Wire_getGeomType() ).toBe(true);
+    });
+	it('test_Wire_getVertices', () => {
+        expect( topos_tests.test_Wire_getVertices() ).toBe(true);
+    });
+	it('test_Wire_getEdges', () => {
+        expect( topos_tests.test_Wire_getEdges() ).toBe(true);
+    });
 });	
 
 describe('Tests for Face class', () => {
-
+	it('test_Face_getGeomType', () => {
+        expect( topos_tests.test_Face_getGeomType() ).toBe(true);
+    });
+	it('test_Face_getVertices', () => {
+        expect( topos_tests.test_Face_getVertices() ).toBe(true);
+    });
+	it('test_Face_getEdges', () => {
+        expect( topos_tests.test_Face_getEdges() ).toBe(true);
+    });
+	it('test_Face_neighbours', () => {
+        expect( topos_tests.test_Face_neighbours() ).toBe(true);
+    });
 });
 
 describe('Tests for Shell class', () => {
--- src/typescript/gs-json/utils/topos_tests.ts
+++ src/typescript/gs-json/utils/topos_tests.ts
@@ -3,3 +3,106 @@ import * as gsj from "./gs-json";
 export function test_xxx():boolean {
     return true;
 }
+
+export function test_Topo_constructor():boolean{
+	return true;
+}
+
+export function test_Topo_getObjID():boolean{
+	return true;
+}
+
+export function test_Topo_getGeom():boolean{
+	return true;
+}
+
+export function test_Topo_getModel():boolean{
+	return true;
+}
+
+export function test_Topo_getGeomType():boolean{
+	return true;
+}
+
+export function test_Topo_getAttribNames():boolean{
+	return true;
+}
+
+export function test_Topo_getAttribValue():boolean{
+	return true;
+}
+
+export function test_Topo_setAttribValue():boolean{
+	return true;
+}
+
+export function test_Topo_getGroupNames():boolean{
+	return true;
+}
+
+
+// Vertex
+export function test_Vertex_getGeomType():boolean{
+	return true;
+}
+
+export function test_Vertex_getPoint():boolean{
+	return true;
+}
+
+export function test_Vertex_next():boolean{
+	return true;
+}
+
+export function test_Vertex_previous():boolean{
+	return true;
+}
+
+export function test_Vertex_getEdge():boolean{
+	return true;
+}
+
+// Edge
+export function test_Edge_getGeomType():boolean{
+	return true;
+}
+export function test_Edge_getVertices():boolean{
+	return true;
+}
+export function test_Edge_next():boolean{
+	return true;
+}
+export function test_Edge_previous():boolean{
+	return true;
+}	
+export function test_Edge_neighbours():boolean{
+	return true;
+}
+export function test_Edge_getParent():boolean{
+	return true;
+}
+
+// Wire
+export function test_Wire_getGeomType():boolean{
+	return true;
+}
+export function test_Wire_getVertices():boolean{
+	return true;
+}
+export function test_Wire_getEdges():boolean{
+	return true;
+}
+
+// Face
+export function test_Face_getGeomType():boolean{
+	return true;
+}
+export function test_Face_getVertices():boolean{
+	return true;
+}
+export function test_Face_getEdges():boolean{
+	return true;
+}
+export function test_Face_neighbours():boolean{
+	return true;
+}
