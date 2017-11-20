b0009045f87d002134a16913b105c8fe97dcb639
Kamel Adouane <sdeka@nus.edu.sg>
2017-11-20 17:21:23 +0800

    adding test structure for Geom and Geom Path classes

--- src/typescript/gs-json/unit-tests/geom.tests.ts
+++ src/typescript/gs-json/unit-tests/geom.tests.ts
@@ -5,4 +5,70 @@ describe('Tests for Geom class', () => {
     it('test_createPoint', () => {
         expect( geom_tests.test_createPoint() ).toBe(true);
     });
-});	
+	it('test_Geom_constructor', () => {
+        expect( geom_tests.test_Geom_constructor() ).toBe(true);
+    });
+	it('test_test_Geom_getModel', () => {
+        expect( geom_tests.test_Geom_getModel() ).toBe(true);
+    });
+	it('test_Geom_addPoint', () => {
+        expect( geom_tests.test_Geom_addPoint() ).toBe(true);
+    });
+	it('test_Geom_addPolyline', () => {
+        expect( geom_tests.test_Geom_addPolyline() ).toBe(true);
+    });
+	it('test_test_Geom_addPolymesh', () => {
+        expect( geom_tests.test_Geom_addPolymesh() ).toBe(true);
+    });
+	it('test_Geom_getPointIDs', () => {
+        expect( geom_tests.test_Geom_getPointIDs() ).toBe(true);
+    });
+	it('test_Geom_getPoints', () => {
+        expect( geom_tests.test_Geom_getPoints() ).toBe(true);
+    });
+	it('test_Geom_getPoint', () => {
+        expect( geom_tests.test_Geom_getPoint() ).toBe(true);
+    });
+	it('test_Geom_delPoint', () => {
+        expect( geom_tests.test_Geom_delPoint() ).toBe(true);
+    });
+	it('test_Geom_numPoints', () => {
+        expect( geom_tests.test_Geom_numPoints() ).toBe(true);
+    });
+	it('test_Geom_setPointPosition', () => {
+        expect( geom_tests.test_Geom_setPointPosition() ).toBe(true);
+    });
+	it('test_Geom_getPointPosition', () => {
+        expect( geom_tests.test_Geom_getPointPosition() ).toBe(true);
+    });
+	it('test_Geom_getObjIDs', () => {
+        expect( geom_tests.test_Geom_getObjIDs() ).toBe(true);
+    });
+	it('test_Geom_getObjs', () => {
+        expect( geom_tests.test_Geom_getObjs() ).toBe(true);
+    });
+	it('test_Geom_getObj', () => {
+        expect( geom_tests.test_Geom_getObj() ).toBe(true);
+    });
+	it('test_Geom_delObj', () => {
+        expect( geom_tests.test_Geom_delObj() ).toBe(true);
+    });
+	it('test_Geom_numObjs', () => {
+        expect( geom_tests.test_Geom_numObjs() ).toBe(true);
+    });
+	it('test_Geom_getTopos', () => {
+        expect( geom_tests.test_Geom_getTopos() ).toBe(true);
+    });
+	it('test_Geom_numTopos', () => {
+        expect( geom_tests.test_Geom_numTopos() ).toBe(true);
+    });
+	it('test_Geom_getAttribTemplate', () => {
+        expect( geom_tests.test_Geom_getAttribTemplate() ).toBe(true);
+    });
+});
+
+describe('Tests for Geom Path class', () => {
+    it('test_GeomPath_constructor', () => {
+        expect( geom_tests.test_GeomPath_constructor() ).toBe(true);
+    });
+});
--- src/typescript/gs-json/utils/geom_tests.ts
+++ src/typescript/gs-json/utils/geom_tests.ts
@@ -11,3 +11,69 @@ export function test_createPoint():boolean {
     if (model.getGeom().numPoints() != 1) {return false;}
     return true;
 }
+
+
+// Geom constructor and its 19 public methods are tested
+export function test_Geom_constructor():boolean {
+    return true;
+}
+export function test_Geom_getModel():boolean {
+    return true;
+}
+export function test_Geom_addPoint():boolean {
+    return true;
+}
+export function test_Geom_addPolyline():boolean {
+    return true;
+}
+export function test_Geom_addPolymesh():boolean {
+    return true;
+}
+export function test_Geom_getPointIDs():boolean {
+    return true;
+}
+export function test_Geom_getPoints():boolean {
+    return true;
+}
+export function test_Geom_getPoint():boolean {
+    return true;
+}
+export function test_Geom_delPoint():boolean {
+    return true;
+}
+export function test_Geom_numPoints():boolean {
+    return true;
+}
+export function test_Geom_setPointPosition():boolean {
+    return true;
+}
+export function test_Geom_getPointPosition():boolean {
+    return true;
+}
+export function test_Geom_getObjIDs():boolean {
+    return true;
+}
+export function test_Geom_getObjs():boolean {
+    return true;
+}
+export function test_Geom_getObj():boolean {
+    return true;
+}
+export function test_Geom_delObj():boolean {
+    return true;
+}
+export function test_Geom_numObjs():boolean {
+    return true;
+}
+export function test_Geom_getTopos():boolean {
+    return true;
+}
+export function test_Geom_numTopos():boolean {
+    return true;
+}
+export function test_Geom_getAttribTemplate():boolean {
+    return true;
+}
+export function test_GeomPath_constructor():boolean {
+    return true;
+}
