b6e5de9c9f7b8c8dbfddb58de25a2f9d26a7bbde
Kamel Adouane <sdeka@nus.edu.sg>
2017-12-01 22:38:11 +0800

    Entities, unit testing

--- src/typescript/gs-json/utils/entities_tests.ts
+++ src/typescript/gs-json/utils/entities_tests.ts
@@ -212,7 +212,45 @@ export function test_Polyline_getObjType():boolean {
 }
 // Polymesh test, extend Obj by 1 method
 export function test_Polymesh_getObjType():boolean {
+    let m:gs.IModel = new gs.Model();
+    let g:gs.IGeom = m.getGeom();
+    let p1 = g.addPoint([0,0,0]);
+    let p2 = g.addPoint([2,0,0]);
+    let p3 = g.addPoint([2,7,0]);
+    let p4 = g.addPoint([7,7,2]);
+    let pmesh1:gs.IPolymesh = g.addPolymesh([
+    [p1,p2,p3],
+    [p2,p4,p3]
+    ]);
 
+    let p10 = g.addPoint([-5,-3,-2]);
+    let p11 = g.addPoint([5,-3,-2]);
+    let p12 = g.addPoint([5,3,-2]);
+    let p13 = g.addPoint([-5,3,-2]);
+    let p14 = g.addPoint([-5,-3,2]);
+    let p15 = g.addPoint([5,-3,2]);
+    let p16 = g.addPoint([5,3,2]);
+    let p17 = g.addPoint([-5,3,2]);
+
+    let pmesh2:gs.IPolymesh = g.addPolymesh([
+    [p13,p12,p11,p10], //bottom
+    [p10,p11,p15,p14], //side0
+    [p11,p12,p16,p15], //side1
+    [p12,p13,p17,p16], //side2
+    [p13,p10,p14,p17], //side3
+    [p14,p15,p16,p17]  //top
+    ]);
+
+    let pmesh3:gs.IPolymesh = g.addPolymesh([
+    [p13,p12,p11,p10], //bottom
+    [p10,p11,p15,p14], //side0
+    [p13,p10,p14,p17], //side3
+    [p14,p15,p16,p17]  //top
+    ]);
+
+    if (!(pmesh1.getObjType() == 200)) {return false;}
+    if (!(pmesh2.getObjType() == 200)) {return false;}
+    if (!(pmesh3.getObjType() == 200)) {return false;}
 
     return true;
 }
