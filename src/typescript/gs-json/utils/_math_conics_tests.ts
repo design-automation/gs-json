d756b8e08ede9359ad8b68c5a7dc12260067b3f6
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-10 18:42:44 +0800

    Circle Evaluate

--- src/typescript/gs-json/utils/_math_conics_tests.ts
+++ src/typescript/gs-json/utils/_math_conics_tests.ts
@@ -23,11 +23,11 @@ export function test_circleLength(): boolean {
 export function test_circleEvaluate(): boolean {
     const m: gs.Model = new gs.Model();
     const g: gs.IGeom = m.getGeom();
-    const pt: gs.IPoint = g.addPoint([5,2,4]);
+    const pt: gs.IPoint = g.addPoint([1,2,9]);
     const angle_1: number = 0;
     const angle_2: number = 360;
-    const vector_x: number[] = [1,0,0];
-    const vector_y: number[] = [0,1,0];
+    const vector_x: number[] = [0,4,0];
+    const vector_y: number[] = [-4,0,0];
     const circle: gs.ICircle = g.addCircle(pt, vector_x,vector_y,[angle_1,angle_2]);
     const threshold: number = 1e-6;
     let xyz: number[] = null;
@@ -35,19 +35,33 @@ export function test_circleEvaluate(): boolean {
     let x: number = null;
     let y: number = null;
     let z: number = null;
+
+    const init_angle: number = Math.PI/2;
     // Evaluation on 99 points on the circle
     for(let k = 1; k<100; k++) {
         xyz = cs.circleEvaluate(circle, 1/k);
         theta = (circle.getAngles()[0] + (circle.getAngles()[1] - circle.getAngles()[0])/k)*2*Math.PI/360;
-        x = circle.getRadius()*Math.cos(theta) + circle.getOrigin().getPosition()[0];
-        y = circle.getRadius()*Math.sin(theta) + circle.getOrigin().getPosition()[1];
+
+        x = circle.getRadius()*Math.cos(theta + init_angle) + circle.getOrigin().getPosition()[0];
+        y = circle.getRadius()*Math.sin(theta + init_angle) + circle.getOrigin().getPosition()[1];
         z = circle.getOrigin().getPosition()[2];
         if(Math.abs(xyz[0] - x) > threshold) {return false ;}
         if(Math.abs(xyz[1] - y) > threshold) {return false ;}
         if(Math.abs(xyz[2] - z) > threshold) {return false ;}
-}
-// To Do: add an extra rotation for considering a circle
-// with which plan is tilted as opposed to parallel to the original plan
+    }
+    const t: number = 0 ;
+    xyz = cs.circleEvaluate(circle, t);
+    theta = (circle.getAngles()[0] + (circle.getAngles()[1] - circle.getAngles()[0])*t)*2*Math.PI/360;
+    // const init_angle: number = Math.PI/2;
+    x = circle.getRadius()*Math.cos(theta + init_angle) + circle.getOrigin().getPosition()[0];
+    y = circle.getRadius()*Math.sin(theta + init_angle) + circle.getOrigin().getPosition()[1];
+    z = circle.getOrigin().getPosition()[2];
+
+    // console.log("xyz is " + xyz)
+
+    if(Math.abs(xyz[0] - x) > threshold) {return false ;}
+    if(Math.abs(xyz[1] - y) > threshold) {return false ;}
+    if(Math.abs(xyz[2] - z) > threshold) {return false ;}
     return true;
 }
 export function test_circleGetRenderXYZs(): boolean {
