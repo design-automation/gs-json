dfed32904ad351a183788b02de2c70a620c61d45
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-19 09:29:53 +0800

    Circle Render XYZ Complete Unit Testing

--- src/typescript/gs-json/utils/_math_conics_tests.ts
+++ src/typescript/gs-json/utils/_math_conics_tests.ts
@@ -66,30 +66,31 @@ export function test_circleGetRenderXYZs(): boolean {
     const m: gs.IModel = new gs.Model();
     const g: gs.IGeom = m.getGeom();
 
-    // In [0,x,y] Plan
+    // [0,x,y]
     const center1: gs.IPoint = g.addPoint([1,1,0]);
     const circle1: gs.ICircle = g.addCircle(center1,[1,0,0],[0,1,0],[0,90]);
     // console.log(cs.circleGetRenderXYZs(circle1,0.4));
     // Valid
 
-    // In [0,x,z] Plan
+    // [0,x,z]
     const center2: gs.IPoint = g.addPoint([1,2,3]);
     const circle2: gs.ICircle = g.addCircle(center2,[1,0,0],[0,0,1],[0,90]);
     // console.log(cs.circleGetRenderXYZs(circle2,0.4));
     // Valid
 
-    // In [0,y,z] Plan
+    // [0,y,z]
     const center3: gs.IPoint = g.addPoint([0,1,2]);
     const circle3: gs.ICircle = g.addCircle(center3,[0,1,0],[0,0,1],[0,90]);
     // console.log(cs.circleGetRenderXYZs(circle3,0.4));
     // Valid
 
-    // In [0,x,(y+z).normalize()] Plan
+    // [0,x,(y+z).normalize()]
     const center4: gs.IPoint = g.addPoint([0,Math.sqrt(2)/2,Math.sqrt(2)/2]);
     const circle4: gs.ICircle = g.addCircle(center4,[1,0,0],[0,Math.sqrt(2)/2,Math.sqrt(2)/2],[0,90]);
     // console.log(cs.circleGetRenderXYZs(circle4,0.4));
+    // Valid
 
-    // In [0,(x+z).normalize()),y] Plan
+    // [0,(x+z).normalize()),y]
     const center5: gs.IPoint = g.addPoint([Math.sqrt(2)/2,0,Math.sqrt(2)/2]);
     const circle5: gs.ICircle = g.addCircle(center5,[Math.sqrt(2)/2,0,Math.sqrt(2)/2],[0,1,0],[0,90]);
     // console.log(cs.circleGetRenderXYZs(circle5,0.4));
@@ -99,6 +100,7 @@ export function test_circleGetRenderXYZs(): boolean {
     const center6: gs.IPoint = g.addPoint([0,-1,0]);
     const circle6: gs.ICircle = g.addCircle(center6,[0,-1,0],[1,0,0],[0,90]);
     // console.log(cs.circleGetRenderXYZs(circle6,0.4));
+    // Valid
     return true;
 }
 export function test_ellipseLength(): boolean {
