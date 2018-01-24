7e4683a17de01f3337441a54fb361d89c85cf8e0
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-24 16:38:55 +0800

    getVectorsEllipse

--- src/typescript/gs-json/utils/entity_obj_ellipse.ts
+++ src/typescript/gs-json/utils/entity_obj_ellipse.ts
@@ -32,7 +32,7 @@ export class Ellipse extends Obj implements IEllipse {
     public getVectors(): XYZ[] {
         // param are [type, x_vec, y_vec, z_vec, angles]
         // return this._kernel.objGetParams(this._id).slice(2,4);
-        return [this._kernel.objGetParams(this._id)[0],this._kernel.objGetParams(this._id)[1]];
+        return [this._kernel.objGetParams(this._id)[3],this._kernel.objGetParams(this._id)[0]];
     }
 
     /**
