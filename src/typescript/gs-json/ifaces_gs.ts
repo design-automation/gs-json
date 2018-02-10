6d7661190806c28cdde9a613dffe92a2591595b4
phtj <patrick@janssen.name>
2018-02-10 11:56:39 +0800

    added copy_attribs

--- src/typescript/gs-json/ifaces_gs.ts
+++ src/typescript/gs-json/ifaces_gs.ts
@@ -70,7 +70,7 @@ export interface IGeom  {
     numPoints(): number;
     mergePoints(points: IPoint[], tolerance?: number): IPoint[];
     mergeAllPoints(tolerance: number): IPoint[];
-    copyPoints(points: IPoint[]): IPoint[];
+    copyPoints(points: IPoint[], copy_attribs: boolean): IPoint[];
     xformPoints(points: IPoint[], matrix: three.Matrix4): void;
     // Objs
     findObjs(obj_type?: EObjType): IObj[];
@@ -80,7 +80,7 @@ export interface IGeom  {
     delObjs(objs: IObj[], keep_points: boolean): boolean;
     delObj(obj: IObj, keep_points: boolean): boolean;
     numObjs(): number;
-    copyObjs(objs: IObj[]): IObj[];
+    copyObjs(objs: IObj[], copy_attribs: boolean): IObj[];
     xformObjs(objs: IObj[], matrix: three.Matrix4): void;
     // Topos
     // getTopos(topo_type: EGeomType): ITopo[];
